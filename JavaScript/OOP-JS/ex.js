0dee5b7a9b0c307d67948f93e1812634084f1ef1
Niranjan Baur <niranjan.baur@bitcanny.com>
2022-09-28 14:15:17 +0530

    js

--- JavaScript/OOP-JS/ex.js
+++ JavaScript/OOP-JS/ex.js
@@ -1,13 +1,45 @@
 class Shape {
-    getColor() {
-        console.log("Red");
+    color = "red"
+    
+     getcolor() {
+        console.log(`Now the color is ${this.color}`);
+        return this;
+    }
+
+     setcolor(newColor){
+        this.color = newColor
+        console.log(`Color name changed to ${this.color}`)
+        return this;
+    }
+    get getColor() {
+        console.log(`Now the color is ${this.color}`);
+        return this;
+    }
+
+    set setColor(newColor){
+        this.color = newColor
+        console.log(`Color name changed to ${this.color}`)
+        return this;
     }
 }
 
 class Rectangle extends Shape {
-
+    static area(len , bred){
+         console.log(2*(len+bred));
+         return this;
+    }
 }
 
 const rec = new Rectangle()
 
-rec.getColor(); 
+rec.getColor;  // getter
+rec.setColor = "blue" // setter
+rec.getColor;  // getter
+
+// ====================================
+// ====================================
+
+// method chaining
+rec.getcolor().setcolor("red").getcolor(); // get and set is not work in method chaining
+
+Rectangle.area(10,5) // call static method using class name
